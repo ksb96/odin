8070e176a447696f97e63237feebfb8bcb1b052f
kubstar <52975077+kubstar@users.noreply.github.com>
2022-09-30 16:01:49 +0530

    basic git command updated

---
+++ git.sh
@@ -0,0 +1,37 @@
+//to change from 'master' to 'main'
+git config --global init.defaultBranch main
+
+// **
+ git branch -m OLD-BRANCH-NAME NEW-BRANCH-NAME
+ git fetch origin
+ git branch -u origin/NEW-BRANCH-NAME NEW-BRANCH-NAME
+ git remote set-head origin -a
+ // **
+
+ // OPTIONAL
+ git remote prune origin
+
+ // git clone
+ git clone git@github.com:USER-NAME/REPOSITORY-NAME.git
+
+ // create a new file
+ touch hello_world.txt
+
+ // to see status of current file
+ git status
+
+// staging to commit
+git add hello_world.txt //file NAME
+
+// commit
+git commit -m "committed"
+
+// log
+git log
+
+// upload/push your code(github) 
+git push 
+OR 
+// for specific branch 
+git push orgin master
+
