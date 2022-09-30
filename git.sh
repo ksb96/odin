ad8d51e0a956ea199174ea09d5ff25043b5cb1af
kubstar <52975077+kubstar@users.noreply.github.com>
2022-09-30 16:05:49 +0530

    comments added

--- git.sh
+++ git.sh
@@ -1,37 +1,42 @@
-//to change from 'master' to 'main'
+# //to change from 'master' to 'main'
 git config --global init.defaultBranch main
 
-// **
+# // **
  git branch -m OLD-BRANCH-NAME NEW-BRANCH-NAME
  git fetch origin
  git branch -u origin/NEW-BRANCH-NAME NEW-BRANCH-NAME
  git remote set-head origin -a
- // **
+#  // **
 
- // OPTIONAL
+#  // OPTIONAL
  git remote prune origin
 
- // git clone
+#  // to exit from current line(in CLI)
+ press 'q' 
+ OR
+ Ctrl + C
+
+#  // git clone
  git clone git@github.com:USER-NAME/REPOSITORY-NAME.git
 
- // create a new file
+#  // create a new file
  touch hello_world.txt
 
- // to see status of current file
+#  // to see status of current file
  git status
 
-// staging to commit
+# // staging to commit
 git add hello_world.txt //file NAME
 
-// commit
+# // commit
 git commit -m "committed"
 
-// log
+# // log
 git log
 
-// upload/push your code(github) 
+# // upload/push your code(github) 
 git push 
 OR 
-// for specific branch 
+# // for specific branch 
 git push orgin master
 
