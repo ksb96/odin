d2a3e35551b288c7035d7e7062123a74a004ace9
kubstar <52975077+kubstar@users.noreply.github.com>
2022-10-25 13:46:59 +0530

    js data types --commit

---
+++ frontend/js/javascript.js
@@ -0,0 +1,34 @@
+// String type
+let name = "ksb";
+
+// Number type
+let number = 122;
+
+// boolean type
+let isMyNamePresent = true;
+if (isMyNamePresent === true) {
+    alert('Name is there')
+} else {
+    console.log("Please assign your name in the above variable, please")
+}
+
+//object & symbol type
+let object ={
+    name: "kub",
+    age: 26,
+    gender: "M"
+};
+
+//typeof 
+typeof 22;
+typeof "kub";
+
+
+// 8 data types
+// number
+// bigint
+// String
+// Boolean
+// null
+// undefined
+// Symbol
