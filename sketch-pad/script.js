833a1d7900d7472a30ad45b6c06a2e2ac8229f7f
kubstar <52975077+kubstar@users.noreply.github.com>
2022-11-07 12:49:13 +0530

    new styling updated --commit

---
+++ sketch-pad/script.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+
+
+    // only draw when mouse is pressed
+    var isDown = false;
+    var firstLoad = true;
+    var size = 8;
+    
+    $(document).on('mousedown', function(){
+      isDown = true;
+    });
+    $(document).on('mouseup', function(){
+      isDown = false;
+    });
+  
+  
+    var clearGrid = function() {
+      $("#pad").children().each(function(){
+        $(this).remove();
+      });
+    };
+  
+    var clearBoxes = function() {
+      $('.box').removeClass('color');
+    };
+  
+  
+    var setListeners = function() {
+      $('#new_grid').on('click', makeGrid);
+      $('#clear').on('click', clearBoxes);
+      $(".box").on('click', function(){
+            $(this).addClass('color');
+      });
+      $(".box").on('mouseover',
+        function(){ 
+          if(isDown) {
+            $(this).addClass('color');
+          }
+      });
+    };
+  
+    var makeGrid = function() {
+      clearGrid();
+      if(firstLoad) {
+        firstLoad = false;
+      } else {
+        size = parseInt(prompt("Enter grid size:"));
+      }
+      for (i=1; i<=size; i++) {
+        $("#pad").append("<div class=\"row\"></div>");
+      }
+      $("#pad").children().each(function() {
+        for (i=1; i<=size; i++) {
+          $(this).append("<div class=\"box\"></div>");
+        }
+      });
+  
+      var dimension = Math.round(parseInt($('#pad').css('height'))/size);
+      console.log(dimension);
+      $('.row').css('height', (dimension)+'px');
+      $('.row').css('width', ((dimension*size)) + 'px');
+      $('.box').css('width', dimension + 'px');
+      $('.box').css('height', dimension + 'px');
+      setListeners();
+    };
+  
+    makeGrid();
+  
+  });
+  
